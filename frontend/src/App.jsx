5600ca9128603aac4fe74433d9bd75b82feaaaeb
Harsh kasana <bt22ece106@gmail.com>
2024-02-18 02:18:01 +0530

    Added Basic structure of Home and EventsPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import Home from "./pages/Home";
+import EventsPage from './pages/EventsPage';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+        <BrowserRouter>
+        <Routes>
+          
+          {/* Pages */}
+          <Route path="/" element={<Home />} />
+          
+          <Route path="/Home" element={<Home />} />
+          <Route path="/EventsPage" element={<EventsPage />} />
+
+          
+          
+        
+
+
+          
+          
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
