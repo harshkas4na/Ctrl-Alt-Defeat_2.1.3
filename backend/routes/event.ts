ff26d6cfdfedb90d14216ace312a0b04491bdd27
armaanansari121 <armaanm2a@gmail.com>
2024-02-19 03:44:48 +0530

    same functionality 2

--- backend/routes/event.ts
+++ backend/routes/event.ts
@@ -7,7 +7,7 @@ const app = express();
 
 app.get('/:category', categoryEventDetails); // Route to get all the events listed in the given category.
 app.get('/', allEvents);             // Route to get all the events without any filter.   
-app.get('/:eventId', eventDetails)
+app.get('/get/:eventId', eventDetails)
 
 
 export default app
