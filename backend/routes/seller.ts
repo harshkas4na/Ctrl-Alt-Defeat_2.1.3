1d0a2ae37c1bcbbdbb69ed47183497dd2d3aebc6
PRakhar <srivastavaprakhar3010@gmail.com>
2024-03-18 15:19:14 +0530

    updated styles of add item form seller login and seller signup

--- backend/routes/seller.ts
+++ backend/routes/seller.ts
@@ -11,7 +11,8 @@ const multer = require('multer');
 const app = express();
 const storage = multer.diskStorage({
     destination: (req:Request, file:any, cb:any) => {
-        cb(null, 'C:/Users/Admin/Desktop/WEBD PROJECTS/CtrlC:/Users/Prakhar Srivastava/OneDrive/Desktop/final blue bit/Ctrl-Alt-Defeat_2.1.3/frontend/public/');
+        cb(null, 'C:/Users/Prakhar Srivastava/OneDrive/Desktop/final blue bit/Ctrl-Alt-Defeat_2.1.3/frontend/public/');
+                       
     },
     filename: (req:Request, file:any, cb:any) => {
         const fileName = file.originalname.toLowerCase().split(' ').join('-');
